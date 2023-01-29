ad8526cc1ecbf5f59bfd93ae18d0046c22d60368
RaspberryDuck <rm.roselymachado@gmail.com>
2023-01-29 12:00:17 -0500

    Added profilePic function and ProfilePicView fucntion

--- AndroidStudio/app/src/main/java/com/example/androidui/MainActivity.kt
+++ AndroidStudio/app/src/main/java/com/example/androidui/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.example.androidui
 
 import android.os.Bundle
+import android.provider.ContactsContract.Profile
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -11,6 +11,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.androidui.ui.theme.AndroidUITheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.ui.res.painterResource
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,26 +26,31 @@ class MainActivity : ComponentActivity() {
         setContent {
             AndroidUITheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                ProfileView()
             }
         }
     }
 }
-
+@Preview
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ProfileView(){
+    Box(modifier = Modifier.fillMaxSize()){
+        Row(modifier=Modifier.align(Alignment.TopEnd).padding(all = 8.dp)){
+            ProfilePicture()
+        }
+    }
 }
 
-@Preview(showBackground = true)
+
+//Profile picture function
 @Composable
-fun DefaultPreview() {
-    AndroidUITheme {
-        Greeting("Android")
-    }
+fun ProfilePicture(){
+    Image(
+        painter = painterResource(R.drawable.roger_profilepic),
+        contentDescription = "Profile Picture",
+        contentScale = ContentScale.Crop,
+        modifier = Modifier
+            .size(65.dp)
+            .clip(CircleShape)
+    )
 }
