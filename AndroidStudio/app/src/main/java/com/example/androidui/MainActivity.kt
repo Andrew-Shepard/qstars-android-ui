ae4098d816d5a4f7e6522496d6b7af90a320b7f3
RaspberryDuck <rm.roselymachado@gmail.com>
2023-03-03 12:02:34 -0500

    minor changes

--- AndroidStudio/app/src/main/java/com/example/androidui/MainActivity.kt
+++ AndroidStudio/app/src/main/java/com/example/androidui/MainActivity.kt
@@ -1,13 +1,10 @@
 package com.example.androidui
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.unit.dp
@@ -18,29 +15,18 @@ import androidx.compose.material.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavGraph
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.semantics.SemanticsActions.OnClick
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MainScreen()
-
         }
     }
 }
--- AndroidStudio/app/src/main/java/com/example/androidui/TopAppBar.kt
+++ AndroidStudio/app/src/main/java/com/example/androidui/TopAppBar.kt
@@ -1,17 +1,10 @@
 package com.example.androidui
 
-import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Lightbulb
 import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.PlusOne
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
@@ -30,11 +23,5 @@ fun TopBar(scope: CoroutineScope, scaffoldState: ScaffoldState)
         },
         backgroundColor = Color.LightGray,
         contentColor = Color.Black,
-        actions = {
-            IconButton(onClick = {}){
-                Icon(Icons.Filled.Add,"")
-            }
-            ProfilePicture()
-        }
     )
 }
